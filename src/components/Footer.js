8189cc19ea77c8c2d9517645893320f52a7d925e
Marat Mustafin <mustafin.m-marat@yandex.ru>
2023-12-06 19:03:14 +0400

    test gh-pages-2

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__subtitle">&copy; 2023 Mesto Russia</p>
-    </footer>
-  );
-}
-
-export default Footer;
